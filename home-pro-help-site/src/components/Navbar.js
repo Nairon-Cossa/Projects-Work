3a0eddd00cb808a697ff1d4620b1e1925ed2066a
Nairon <nmcossa@gmail.com>
2025-07-08 21:36:58 +0200

    changes 1

--- home-pro-help-site/src/components/Navbar.js
+++ home-pro-help-site/src/components/Navbar.js
@@ -1,21 +1,34 @@
-import React from 'react';
+// src/components/Navbar.js
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/LogoHPH.png';
 import './Navbar.css';
+
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="navbar">
-      <div>
+      <div className="navbar-container">
         <img src={logo} alt="Logo" className="navbar-logo" />
-      </div>
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/sobre-nos">Sobre Nós</Link>
-        <Link to="/servicos">Serviços</Link>
-        <Link to="/projectos">Projectos</Link>
-        <Link to="/team">Team</Link>
-        <Link to="/contacte">Contacte</Link>
-        <Link to="/app">App</Link>
+
+        {/* Hamburger icon (only on mobile) */}
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        {/* Links */}
+        <div className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link to="/sobre-nos" onClick={() => setMenuOpen(false)}>Sobre Nós</Link>
+          <Link to="/servicos" onClick={() => setMenuOpen(false)}>Serviços</Link>
+          <Link to="/projectos" onClick={() => setMenuOpen(false)}>Projectos</Link>
+          <Link to="/team" onClick={() => setMenuOpen(false)}>Team</Link>
+          <Link to="/contacte" onClick={() => setMenuOpen(false)}>Contacte</Link>
+          <Link to="/app" onClick={() => setMenuOpen(false)}>App</Link>
+        </div>
       </div>
     </nav>
   );
